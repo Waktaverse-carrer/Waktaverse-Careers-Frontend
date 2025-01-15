dbdf3c4594530004de03877991fa0ffdfba6759a
oko_jin <dswvgw6412@naver.com>
2025-01-15 20:41:43 +0900

    :lipstick: feat: 태그 컴포넌트 UI 구현

--- src/app/template/_components/Tag.tsx
+++ src/app/template/_components/Tag.tsx
@@ -1,16 +1,18 @@
 import styled from 'styled-components';
+import { getStandardColor } from '@/app/lib/constants/getColor';
+import { SaturationType } from '@/app/lib/types/colors';
 
-type ColorMapType = {
-  [key: string]: string;
-};
-
-const ColorMap: ColorMapType = {
-  비챤: '#7cc018',
-  크레딧: '#1dcbae',
-  일러스트: '#7832b9',
+const memberObj: Record<string, string> = {
+  아이네: 'ine',
+  징버거: 'jingburger',
+  릴파: 'lilpa',
+  주르르: 'jururu',
+  고세구: 'gosegu',
+  비챤: 'viichan',
 };
 
 interface TagStyleProps {
+  type: string;
   text: string;
 }
 
@@ -20,13 +22,19 @@ const TagStyle = styled.div<TagStyleProps>`
   border-radius: 5px;
   color: white;
   font-weight: 500;
-  background-color: ${(props) => ColorMap[props.text]};
+  background-color: ${({ type, text }) =>
+    getStandardColor(type, text as SaturationType)};
 `;
 
 export interface Props {
+  type: string;
   text: string;
 }
 
-export default function Tag({ text }: Props) {
-  return <TagStyle text={text}>{text}</TagStyle>;
+export default function Tag({ type, text }: Props) {
+  return (
+    <TagStyle type={type} text={memberObj[text]}>
+      {text}
+    </TagStyle>
+  );
 }
