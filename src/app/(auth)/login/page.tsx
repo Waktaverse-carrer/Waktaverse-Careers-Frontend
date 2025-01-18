a19ec5345109a0d7b50cce9593b03ecd7a5a021b
freevuehub <hackish113@gmail.com>
2025-01-18 13:44:37 +0900

    feat: 세션 관리 영역 분리

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,13 @@
+// import styled from 'styled-components';
+import { AuthButton } from '@/components';
+
+const Login = () => {
+  return <AuthButton.Naver />;
+};
+
+// const Wrap = styled.div`
+//   width: 100vw;
+//   height: 100vh;
+// `;
+
+export default Login;
