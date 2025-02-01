2dd05454b037aea9bd82e7959801519ad07a75d4
GulSam00 <gulsamcono@gmail.com>
2025-02-02 01:27:42 +0900

    feat : 뼈대 제작

---
+++ src/app/store/page.tsx
@@ -0,0 +1,43 @@
+'use client';
+
+import { useState } from 'react';
+
+import SearchPanel from './_components/SearchPanel';
+import Layout from './_components/Layout';
+
+import styled from 'styled-components';
+
+const Home = () => {
+  const [search, setSearch] = useState<string>('');
+  const [tags, setTags] = useState<string[]>([]);
+
+  return (
+    <Layout>
+      <StoreTitle>
+        <div className='text'>템플릿</div>
+        <img src='images/main-banner-1.png' alt='main-banner' />
+      </StoreTitle>
+      <StoreContent>
+        <SearchPanel {...{ search, setSearch, tags, setTags }} />
+      </StoreContent>
+    </Layout>
+  );
+};
+
+const StoreTitle = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: left;
+
+  width: 100%;
+  gap: 30px;
+
+  .text {
+    font-size: 50px;
+    font-weight: 600;
+  }
+`;
+
+const StoreContent = styled.div``;
+
+export default Home;
